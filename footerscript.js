ab5580e4801934811609ea0c286bf230ee746547
Arthur <arthur.zulpukharov.simplon@gmail.com>
2024-01-25 16:22:11 +0100

    footer

--- footerscript.js
+++ footerscript.js
@@ -1,31 +1,23 @@
 const facebook = document.getElementById("facebook");
-
 facebook.addEventListener("mouseover" , () => {
     facebook.classList.add("zoomed");
 });
-
 facebook.addEventListener("mouseout" , () => {
     facebook.classList.remove("zoomed");
 });
 
 const twitter = document.getElementById("twitter");
-
 twitter.addEventListener("mouseover" , () => {
     twitter.classList.add("zoomed");
 });
-
 twitter.addEventListener("mouseout" , () => {
     twitter.classList.remove("zoomed");
 });
 
 const instagram = document.getElementById("instagram");
-
-
-
 instagram.addEventListener("mouseover" , () => {
     instagram.classList.add("zoomed");
 });
-
 instagram.addEventListener("mouseout" , () => {
     instagram.classList.remove("zoomed");
 });
